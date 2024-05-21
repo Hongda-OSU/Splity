d0ae43a2cc3e88480002c3c767de064ef911b8b7
Hongda <67817916+Hongda-OSU@users.noreply.github.com>
2024-05-21 02:21:03 -0500

    fix bug in websocket

--- splity/src/helper/useWebsocket.js
+++ splity/src/helper/useWebsocket.js
@@ -1,12 +1,26 @@
 import { useEffect } from "react";
 import { splity_websocket } from "./api";
+import { useMyStore } from "@/store/store";
 
 const useWebSocket = (setPaymentHistory, setPaymentReceived) => {
+  const { billCreator } = useMyStore((state) => ({
+    billCreator: state.billCreator,
+  }));
+
   useEffect(() => {
+    if (!billCreator.bill_id) return;
+    
     const ws = new WebSocket(splity_websocket);
 
     ws.onopen = () => {
       console.log("Connected to WebSocket");
+      ws.send(
+        JSON.stringify({ action: "set_bill_id", bill_id: billCreator.bill_id })
+      );
+    };
+
+    ws.onerror = (error) => {
+      console.error("WebSocket Error: ", error);
     };
 
     ws.onmessage = (event) => {
@@ -25,7 +39,7 @@ const useWebSocket = (setPaymentHistory, setPaymentReceived) => {
     return () => {
       ws.close();
     };
-  }, [setPaymentHistory, setPaymentReceived]);
+  }, [setPaymentHistory, setPaymentReceived, billCreator.bill_id]);
 };
 
 export default useWebSocket;
